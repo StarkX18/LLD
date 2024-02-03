f69f456b2a84f4c5aab01d0a903ca1a797afe489
StarkX18 <siddharth.25@icloud.com>
2024-02-03 15:19:51 +0530

    remake prototype

--- design-patterns/creationalDP/prototype/src/IntelligentStudent.java
+++
@@ -1,7 +0,0 @@
-public class IntelligentStudent extends Student{
-    public IntelligentStudent(){};
-    public IntelligentStudent(IntelligentStudent is){
-        super(is);
-        this
-    }
-}
